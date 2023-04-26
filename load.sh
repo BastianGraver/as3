78f93ffbbc95885499fcf95c57f95b058698eda7
tobias0106 <62556939+tobias0106@users.noreply.github.com>
2023-04-26 14:05:20 +0200

    Create load.sh

---
+++ load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+./unload.sh
+
+make
+# create directory
+mkdir /tmp/lfs-mountpoint
+
+# set directory permissions
+chmod 755 /tmp/lfs-mountpoint/
+
+# mount LFS to directory
+./lfs /tmp/lfs-mountpoint/ -d -f
+
+# change to mount directory
+cd /tmp/lfs-mountpoint/
